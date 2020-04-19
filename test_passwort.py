dcca8933f955b6b88b12f3da8f60582586a39f81
Mary Waithera <njihiamary11@gmail.com>
2020-04-19 14:36:24 +0300

    Create an initialization test case to check if a new object is inialized properly

--- test_passwort.py
+++ test_passwort.py
@@ -8,3 +8,23 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.Testcase: TestCase class to facilitate the creation of test units
     '''
+
+    def setUp(self):
+        '''
+        SetUp() method defines the instructions that will be excuted before each test method
+        '''
+        self.new_user = User("Mary", "Njihia", "hgnkf254")
+
+    #Test if it possible to create a new user
+    def test_init(self):
+        '''
+        test_init test case checks if object is initatilized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name, "Mary")
+        self.assertEqual(self.new_user.last_name, "Njihia")
+        self.assertEqual(self.new_user.password, "hgnkf254")
+
+
+if __name__ == '__main__':
+    unittest.main()
